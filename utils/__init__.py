275de216c4f31def4f7f478495fa495c17b2da43
EAJ-APP <ealamillos91@gmail.com>
2025-09-26 13:47:12 +0200

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,11 @@
+# Importar desde los archivos correctos
 from .error_handling import handle_bq_error, check_dependencies
 from .helpers import setup_environment, format_percentage, format_currency, safe_divide
 
 __all__ = [
     'handle_bq_error', 
-    'check_dependencies', 
-    'setup_environment',
+    'check_dependencies',  # Esta viene de error_handling.py
+    'setup_environment',   # Esta viene de helpers.py
     'format_percentage', 
     'format_currency', 
     'safe_divide'
