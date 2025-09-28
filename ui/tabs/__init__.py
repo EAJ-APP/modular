abdcfc7436a5bba3b2ac998b17d7c66bbc3c974a
EAJ-APP <ealamillos91@gmail.com>
2025-09-28 10:56:55 +0200

    Update __init__.py

--- ui/tabs/__init__.py
+++ ui/tabs/__init__.py
@@ -1,4 +1,5 @@
 from .cookies_tab import show_cookies_tab
 from .ecommerce_tab import show_ecommerce_tab
+from .acquisition_tab import show_acquisition_tab
 
-__all__ = ['show_cookies_tab', 'show_ecommerce_tab']
+__all__ = ['show_cookies_tab', 'show_ecommerce_tab','show_acquisition_tab']
