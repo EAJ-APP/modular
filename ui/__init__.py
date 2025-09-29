6b733ea302ff67ef92e1ff5d3c713a10902baf68
EAJ-APP <ealamillos91@gmail.com>
2025-09-29 13:02:46 +0200

    Update __init__.py

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,10 +1,11 @@
 from .sidebar import render_sidebar, get_project_dataset_selection
-from .tabs import show_cookies_tab, show_ecommerce_tab, show_acquisition_tab
+from .tabs import show_cookies_tab, show_ecommerce_tab, show_acquisition_tab, show_events_tab
 
 __all__ = [
     'render_sidebar', 
     'get_project_dataset_selection',
     'show_cookies_tab', 
     'show_ecommerce_tab',
-    'show_acquisition_tab'
+    'show_acquisition_tab',
+    'show_events_tab'
 ]
