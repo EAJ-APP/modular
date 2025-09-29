2c6871f13da96e4ce39e4deb4b1bbd23a6503d56
EAJ-APP <ealamillos91@gmail.com>
2025-09-29 18:18:51 +0200

    Update __init__.py

--- ui/tabs/__init__.py
+++ ui/tabs/__init__.py
@@ -3,11 +3,13 @@ from .ecommerce_tab import show_ecommerce_tab
 from .acquisition_tab import show_acquisition_tab
 from .events_tab import show_events_tab
 from .users_tab import show_users_tab
+from .sessions_tab import show_sessions_tab
 
 __all__ = [
     'show_cookies_tab',
     'show_ecommerce_tab',
     'show_acquisition_tab',
     'show_events_tab',
-    'show_users_tab'
+    'show_users_tab',
+    'show_sessions_tab'
 ]
